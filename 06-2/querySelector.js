8207c306d441be117c0678369e3066b13e44997f
kweonsikyung <ekrk6632@naver.com>
2022-07-21 02:48:03 +0900

    자바스크립트로 웹페이지 제어하기

---
+++ 06-2/querySelector.js
@@ -0,0 +1,14 @@
+/* 요소를 1개만 선택하기 */
+/*document.querySelector("#wrap");*/
+
+
+/* 변수를 만들어서 요소 선택하기  */
+/*절대 변경해서는 안되는 변수값은 const 예약어로 지정*/
+
+const frame = document.querySelector("#wrap");
+console.log(frame);
+
+/* 클래스명이 box1인 요소 선택하기 
+const box1 = document.querySelector("#wrap .box1");
+console.log(box1); 
+*/
