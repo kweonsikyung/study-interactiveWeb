da1c30697ca601f50cdc89e8886c6dbf16e9a7b3
kweonsikyung <ekrk6632@naver.com>
2022-07-28 23:52:35 +0900

    자바스크립트로 웹페이지 제어하기

--- ch06_7/main.js
+++ ch06_7/main.js
@@ -3,5 +3,5 @@ const link_href = link.getAttribute("href");
 console.log(link_href);
 
 // 속성값 변경하기
-//                                                                                                                                                                                                                                                                                                                        
+// git수정용                                                                                                                                                                                                                                                                                                     
  
