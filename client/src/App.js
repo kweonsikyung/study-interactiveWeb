5ab67f0a62747bf217514ed8a3a7627cc892a99c
kweonsikyung <ekrk6632@naver.com>
2022-08-05 01:38:55 +0900

    interactive_web

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import LifecycleEx from './R004_LifecycleEx';
+//import ImportComponent from './R003_ImportComponent';
 
 function App() {
   return (
     <div>
       <h1>START REACT 200!</h1>
       <p>HTML 적용하기</p>
+      <LifecycleEx></LifecycleEx>
     </div>
   );
 }
