2471cbaef6d5d1d900d49173cc564ad81225415c
kweonsikyung <ekrk6632@naver.com>
2022-08-22 03:43:34 +0900

    프래그먼트

--- client/src/R032_Fragments.js
+++ client/src/R032_Fragments.js
@@ -1,7 +1,10 @@
-import React, {Component} from "react";
-//Fragments 이용하기
+//Fragments 이용하기 
 //element 반환 시 하나의 <html>태그로 감싸지 않으면 에러메세지가 뜬다
 //이때 <Fragments> 태그로 감싸면 불필요한 <html>태그를 추가하지 않고 사용할 수 있다
+
+import React, {Component} from "react";
+
+
 class R032_Fragments extends Component{
     render() {
         return(
