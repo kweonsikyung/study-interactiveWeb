f9923a64bf7c79ae1f47a9b265b67546c4183508
kweonsikyung <ekrk6632@naver.com>
2022-08-24 03:10:34 +0900

    reactstrap 사용하기

--- client/src/R041_ReactstrapCarousel.js
+++ client/src/R041_ReactstrapCarousel.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {UncontrolledCarousel} from 'reactstrap';
 
+//캐러셀
 // Carousel 패키지는 슬라이드를 자동으로 회전시키는 기능을 제공한다.
 //이미지 텍스트, 버튼 등으로 이루어진 슬라이드를 일정 단위로 다음 슬라이드로 이동시킨다
 
