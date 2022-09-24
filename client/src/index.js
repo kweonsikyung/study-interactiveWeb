20494796b53e5bf2606da215c3fad51023906c8c
kweonsikyung <ekrk6632@naver.com>
2022-09-24 23:27:39 +0900

    redux 사용하기

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import createStore from 'redux';
+import reducers from './reducers';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//createStore 함수의 파라미터로 reducers 폴더 경로를 넘긴다
+const store = createStore(reducers);
+
+//위에서 생성한 store를 App컴포넌트에 전달한다. 
+//render함수를 listener라는 함수 내부에 위치시킨다.
+const listener = () => {
+  ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
+  );
+};
+
+//store를 구독하면 store 데이터에 변화가 있을 때 listener 함수 내부의 render함수를 실행하고 
+//변경된 데이터를 렌더링한다
+store.subscribe(listener);
+//위에서 render함수를 listener 함수로 감쌌기 때문에 
+//초기 렌더링을 위해 수동으로 render 함수를 실행시켜준다
+listener();
