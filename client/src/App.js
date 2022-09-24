6e1b5a1841919014c57f0ab47f55b8b5192afa8e
kweonsikyung <ekrk6632@naver.com>
2022-09-24 17:10:58 +0900

    redux 설치

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,47 @@
 import React from "react";
 // import './App.css';
-import ReactstrapAlerts from './R034_ReactstrapAlerts'
-import ReactstrapBadges from './R035_ReactstrapBadges'
-import ReactstrapBreadcrumbs from './R036_ReactstrapBreadcrumbs'
-import ReactstrapDropDown from './R037_ReactstrapDropDown'
-import ReactstrapButtonGroup from './R038_ReactstrapButtonGroup'
-import ReactstrapBottons from './R039_ReactstrapBottons'
-import ReactstrapCard from './R040_ReactstrapCard'
-import ReactstrapCarousel from './R041_ReactstrapCarousel'
-import ReactstrapCollapse from './R042_ReactstrapCollapse'
-import ReactstrapFade from './R043_ReactstrapFade'
-import ReactstrapForm from './R044_ReactstrapForm'
-import ReactstrapInputGroup from './R045_ReactstrapInputGroup'
-import ReactstrapJumbotron from './R046_ReactstrapJumbotron'
-import ReactstrapListGroup from './R047_ReactstrapListGroup'
-import ReactstrapModal from './R048_ReactstrapModal'
-import ReactstrapNavbar from './R049_ReactstrapNavbar'
-import ReactstrapPagination from './R050_ReactstrapPagination'
-import ReactstrapPopover from './R051_ReactstrapPopover'
-import ReactstrapProgress from './R052_ReactstrapProgress'
-import ReactstrapSpinner from './R053_ReactstrapSpinner'
-import ReactstrapTable from './R054_ReactstrapTable'
-import ReactstrapTab from './R055_ReactstrapTab'
+// import ReactstrapAlerts from './R034_ReactstrapAlerts'
+// import ReactstrapBadges from './R035_ReactstrapBadges'
+// import ReactstrapBreadcrumbs from './R036_ReactstrapBreadcrumbs'
+// import ReactstrapDropDown from './R037_ReactstrapDropDown'
+// import ReactstrapButtonGroup from './R038_ReactstrapButtonGroup'
+// import ReactstrapBottons from './R039_ReactstrapBottons'
+// import ReactstrapCard from './R040_ReactstrapCard'
+// import ReactstrapCarousel from './R041_ReactstrapCarousel'
+// import ReactstrapCollapse from './R042_ReactstrapCollapse'
+// import ReactstrapFade from './R043_ReactstrapFade'
+// import ReactstrapForm from './R044_ReactstrapForm'
+// import ReactstrapInputGroup from './R045_ReactstrapInputGroup'
+// import ReactstrapJumbotron from './R046_ReactstrapJumbotron'
+// import ReactstrapListGroup from './R047_ReactstrapListGroup'
+// import ReactstrapModal from './R048_ReactstrapModal'
+// import ReactstrapNavbar from './R049_ReactstrapNavbar'
+// import ReactstrapPagination from './R050_ReactstrapPagination'
+// import ReactstrapPopover from './R051_ReactstrapPopover'
+// import ReactstrapProgress from './R052_ReactstrapProgress'
+// import ReactstrapSpinner from './R053_ReactstrapSpinner'
+// import ReactstrapTable from './R054_ReactstrapTable'
+// import ReactstrapTab from './R055_ReactstrapTab'
 
-import FetchGet from './R059_FetchGet';
-import FetchPost from './R060_FetchPost';
-import AxiosGet from './R061_AxiosGet';
-import AxiosPost from './R062_AxiosPost';
+// import FetchGet from './R059_FetchGet';
+// import FetchPost from './R060_FetchPost';
+// import AxiosGet from './R061_AxiosGet';
+// import AxiosPost from './R062_AxiosPost';
 
-import CallbackFunc from './R063_CallbackFunc'
-import Promise1 from './R064_Promise'
-import Promise2 from './R065_Promise'
+// import CallbackFunc from './R063_CallbackFunc'
+// import Promise1 from './R064_Promise'
+// import Promise2 from './R065_Promise'
 
-import ReactonClick from './R066_onClick'
-import ReactChange from './R067_onChange'
-import ReactMouseMove from './R068_onMouseMove'
-import ReactMouseOver from './R069_onMouseOver'
-import ReactMouseOut from './R070_onMouseOut'
-import ReactonKey from './R071_OnKey'
-import ReactonSubmit from './R072_onSubmit'
-import ReactRef from './R073_ReactRef'
-import Currying from './R074_ReactCurrying'
-import ReactHoc from './R075_ReactHoc'
+// import ReactonClick from './R066_onClick'
+// import ReactChange from './R067_onChange'
+// import ReactMouseMove from './R068_onMouseMove'
+// import ReactMouseOver from './R069_onMouseOver'
+// import ReactMouseOut from './R070_onMouseOut'
+// import ReactonKey from './R071_OnKey'
+// import ReactonSubmit from './R072_onSubmit'
+// import ReactRef from './R073_ReactRef'
+// import Currying from './R074_ReactCurrying'
+// import ReactHoc from './R075_ReactHoc'
 // import ContextApi from './Context/R076_ContextApi'
 // import ContextApi from './Context/R077_ContextApi'
 
@@ -52,7 +52,7 @@ function App() {
   return (
     <div>
       <h1>START REACT 200!</h1>
-      <p>CSS 적용하기</p>
+      {/* <p>CSS 적용하기</p> */}
       {/* R019 예제 
       <PropsBoolean BooleanTrueFalse = {false}/>
       <PropsBoolean BooleanTrueFalse />
@@ -109,7 +109,7 @@ function App() {
       {/* <Promise2/> */}
 
 
-      <ReactonClick></ReactonClick>
+      {/* <ReactonClick></ReactonClick>
       <div></div>
       <ReactChange></ReactChange>
       <div></div>
@@ -127,10 +127,13 @@ function App() {
       <Currying></Currying>
      <div></div>
       <ReactHoc name='React200'></ReactHoc>
-       <div></div>
-       {/* fail
+       <div></div> */}
+
+       {/* fail-contextApi 다시 살펴보기
       <ContextApi></ContextApi> */}
       {/* <ContextApi></ContextApi> */}
+
+      
     </div>
   );
 }
