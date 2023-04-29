98ebe27ffe166f39f8aa82de019df7f96f648198
kweonsikyung <ekrk6632@naver.com>
2023-04-29 13:48:49 +0900

    add: customHooks

--- hook-master/src/App.js
+++ hook-master/src/App.js
@@ -5,9 +5,19 @@ import Attendance from './component/Attendance';
 import Ref from './example/useRef';
 import RefCount from './component/Count';
 import Focus from './component/Focus';
+import { useInput } from './customHook/useInput';
+import { useToggle } from './customHook/useToggle';
 
+function displayMessage(message) {
+  alert(message);
+
+}
 function App() {
   const [showTimer, setShowTimer] = useState(false);
+
+  const [inputValue, handleChange, handleSubmit] = useInput("",displayMessage );
+  const [isTextChanged, setIsTextChanged] = useToggle();
+  const [isTextChanged1, setIsTextChanged1] = useToggle();
 
   return (
     <div> 
@@ -21,6 +31,15 @@ function App() {
       <Ref />
       <RefCount />
       <Focus />
+
+      {/* customHook */}
+      <h1>useInput</h1>
+      <input value={inputValue} onChange={handleChange} />
+      <button onClick={handleSubmit}>확인</button>
+
+      <h1>useToggle</h1>
+      <button onClick={setIsTextChanged}>{isTextChanged ? 'Toggled' : 'Click to Toggle'}</button>
+      <button onClick={setIsTextChanged1}>{isTextChanged1 ? 'Hi!' : 'Hello!'}</button>
     </div>
     
 
